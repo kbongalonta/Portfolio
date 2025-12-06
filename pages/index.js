3863a6a77080c0a3f6cdc525ea338ba3598bb16c
kbongalonta <carlabongalonta34@gmail.com>
2025-12-06 14:47:23 +0800

    new

--- pages/index.js
+++ pages/index.js
@@ -134,7 +134,7 @@ export default function Home() {
               <h3 className="heading-technical">Technical Skills</h3>
               <img
                 className="background-shadow-7"
-                src="/your-image.png"
+                src="/Background+Shadow (1).png"
                 alt="Technical skills chart showing proficiency in HTML, CSS, JavaScript, React, and other web technologies"
               />
             </div>
